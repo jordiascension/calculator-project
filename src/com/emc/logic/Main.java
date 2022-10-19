8818504fb819df1495623ee7fe1964363958c0ec
jordiascension <jordiascension@enterpriseconsulting.es>
2022-10-19 13:02:41 +0200

    Added Main class with printout implemented

---
+++ src/com/emc/logic/Main.java
@@ -0,0 +1,14 @@
+package com.emc.logic;
+
+public class Main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Calculator calculator = new Calculator();
+		int addResult = calculator.add(4, 8);
+
+		System.out.println("El resultado de la suma es " + addResult);
+
+	}
+
+}
