e3a5f1571ddf7be481716fad98b12c1b6129de20
jordiascension <jordiascension@enterpriseconsulting.es>
2022-10-24 11:28:19 +0200

    Added Prime Numbers implementation. closes #1

---
+++ src/com/emc/primenumbers/PrimeNumbersTest.java
@@ -0,0 +1,38 @@
+package com.emc.primenumbers;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
+
+import java.util.ArrayList;
+
+import org.junit.jupiter.api.Test;
+
+class PrimeNumbersTest {
+
+	PrimeNumbers primeNumbers = new PrimeNumbers();
+
+	@Test
+	void testSearchPrimeNumbers() {
+
+		int[] numbers = new int[4];
+		numbers[0] = 15;
+		numbers[1] = 16;
+		numbers[2] = 17;
+		numbers[3] = 18;
+
+		ArrayList<Integer> primeArray = primeNumbers
+				.SearchPrimeNumbers(numbers);
+
+		ArrayList<Integer> primeArrayExpected = new ArrayList<Integer>();
+		primeArrayExpected.add(17);
+		assertEquals(primeArray, primeArrayExpected);
+	}
+
+	@Test
+	void testEsPrimo() {
+
+		assertTrue(primeNumbers.isPrime(17) == true);
+
+	}
+
+}
