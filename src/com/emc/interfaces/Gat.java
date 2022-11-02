1a39ffe0e66cb594a6252e920b1db3282dfe1b7c
jordiascension <jordiascension@enterpriseconsulting.es>
2022-11-02 10:21:56 +0100

    Added interface exercise. closes #6

---
+++ src/com/emc/interfaces/Gat.java
@@ -0,0 +1,11 @@
+package com.emc.interfaces;
+
+public class Gat implements Animal {
+
+	@Override
+	public void descripcioAnimal() {
+		// TODO Auto-generated method stub
+
+	}
+
+}
---
+++ src/com/emc/interfaces/MyAbstractClass.java
@@ -0,0 +1,10 @@
+package com.emc.interfaces;
+
+public abstract class MyAbstractClass {
+
+	public abstract void method1();
+
+	public void method2() {
+		System.out.println("This a none abstract method");
+	}
+}
